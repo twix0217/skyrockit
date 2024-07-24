eb83869c161edbb3f4781a8bc4887ce678a36624
twix217 <hussainbarakat02@gmail.com>
2024-07-24 14:35:59 +0300

    Build application schema, build a home page

--- models/user.js
+++ models/user.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+  company: String,
+  title: String,
+  date: Date,
+  notes: String,
+  postingLink: String,
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted']
+  }
+}, { timestamps: true })
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +21,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema]
 });
 
 const User = mongoose.model('User', userSchema);
